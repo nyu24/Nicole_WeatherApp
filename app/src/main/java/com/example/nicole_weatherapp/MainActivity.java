8abb3d9139d1f954607bea409c2eda6c7daff1a3
nyu24 <nyu24@tfs.ca>
2022-11-30 11:04:55 -0500

    Etape 2

--- app/src/main/java/com/example/nicole_weatherapp/MainActivity.java
+++ app/src/main/java/com/example/nicole_weatherapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.nicole_weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,14 +19,18 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     //les champs
-    TextView mDate, mVille, mTemp, mDescription;
+    TextView mDate, mVille, mTemp, mDescription, mVent, mTempF;
     ImageView imgIcon;
     String maVille = "Toronto";
 
@@ -34,26 +45,91 @@ public class MainActivity extends AppCompatActivity {
         mVille = findViewById(R.id.mVille);
         mTemp = findViewById(R.id.mTemp);
         mDescription = findViewById(R.id.mDescription);
+        mVent = findViewById(R.id.mVent);
+        mTempF = findViewById(R.id.mTempF);
 
-        requestQueue = Volley.newRequestQueue(this);
         afficher();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.recherche, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Ecrire le nom de la ville");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                maVille = query;
+                afficher();
+                //gestion du clavier
+                InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                if(getCurrentFocus() != null) {
+                    inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void afficher(){
-        String url = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=3ddc142c3a3b1aed34e0a885a5af9119&units=metric";
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + maVille + "&appid=3ddc142c3a3b1aed34e0a885a5af9119&units=metric";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    //afficher dans le logcat
                     JSONObject main_object = response.getJSONObject("main");
+                    JSONObject wind_object = response.getJSONObject("wind");
                     JSONArray array = response.getJSONArray("weather");
+                    JSONObject object = array.getJSONObject(0);
+
+                    //afficher dans le logcat
                     //afficher weather
-                    Log.d("Tag", "resultat =" + array.toString());
+                    //Log.d("Tag", "resultat =" + array.toString());
                     //afficher main : temperature (feels like, min, max), pression, humidite.
-                    Log.d("Tag", "resultat =" + main_object);
+                    //Log.d("Tag", "resultat =" + main_object);
+                    //noter la temperature
+
+                    int tempC = (int) Math.round(main_object.getDouble("temp"));
+                    String temp = String.valueOf(tempC);
+
+                    int tempFeels = (int) Math.round(main_object.getDouble("feels_like"));
+                    String tempF = String.valueOf(tempFeels);
+
+                    int spdV = (int) Math.round(wind_object.getDouble("speed"));
+                    String spd = String.valueOf(spdV);
+
+                    String description = object.getString("description");
+                    String city = response.getString("name");
+                    String icon = object.getString("icon");
+
+                    //mettre dans les champs
+                    mVille.setText(city);
+                    mTemp.setText(temp);
+                    mDescription.setText(description);
+                    mVent.setText(spd);
+                    mTempF.setText(tempF);
+
+                    //formattage du temps
+                    Calendar calendar = Calendar.getInstance();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, MMMM dd");
+                    String formatted_date = simpleDateFormat.format(calendar.getTime());
+                    mDate.setText(formatted_date);
+
+                    //l'image
+                    String imageUri = "http://openweathermap.org/img/w/" + icon + ".png";
+                    imgIcon = findViewById(R.id.imgIcon);
+                    Uri myUri = Uri.parse(imageUri);
+                    Picasso.with(MainActivity.this).load(myUri).resize(200,200).into(imgIcon);
+
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -66,6 +142,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        //ajouter les elements dans une queue
+        requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(jsonObjectRequest);
         }
 
